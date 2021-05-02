e492dc33566d7bf81f444501b2797e8314ad43aa
yoonhe <yhehe228@gmail.com>
2021-05-02 14:53:28 +0900

    mata-config 파일을 수정합니다

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Heaeun`,
+  description: `프론트엔드 개발자 윤해은입니다`,
+  author: `Heaeun`,
+  introduction: `코드리뷰, TDD, 함께 자라기를 좋아하는 프론트엔드 개발자입니다`,
+  siteUrl: `https://yoonhe.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/yoonhe`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
     instagram: ``, // Your Instagram account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/profile.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'yoonhe', // Your disqus-short-name. check disqus.com.
+    utterances: 'yoonhe/yoonhe.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'yoonhe',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
